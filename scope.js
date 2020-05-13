18a0aad07632dec5e9cacc4a8db2fa4ff5d71e79
Wryhder <jemimahsummers@yahoo.com>
2020-05-13 12:45:57 +0100

    Add new operators and function values to global scope

--- scope.js
+++ scope.js
@@ -3,4 +3,13 @@ const topScope = Object.create(null);
 topScope.true = true;
 topScope.false = false;
 
+for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
+    topScope[op] = Function("a, b", `return a ${op} b;`);
+}
+
+topScope.print = value => {
+    console.log(value);
+    return value;
+};
+
 export default topScope;
