1dde50aba2ce0ac9836f27b455283629c0c25ba1
Wryhder <jemimahsummers@yahoo.com>
2020-05-13 18:13:08 +0100

    Add basic support for arrays (construction, length, fetch nth element)

--- scope.js
+++ scope.js
@@ -3,7 +3,7 @@ const topScope = Object.create(null);
 topScope.true = true;
 topScope.false = false;
 
-for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
+for (let op of ["+", "*", "/", "==", "<", ">"]) {
     topScope[op] = Function("a, b", `return a ${op} b;`);
 }
 
@@ -11,5 +11,17 @@ topScope.print = value => {
     console.log(value);
     return value;
 };
+
+topScope.array = (...values) => {
+    return [...values];
+}
+
+topScope.length = array => {
+    return array.length;
+}
+
+topScope.element = (array, n) => {
+    return array[n];
+}
 
 export default topScope;
