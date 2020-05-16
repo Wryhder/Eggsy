6a25215cc75a07d249f975906fd611edac414c0c
Wryhder <jemimahsummers@yahoo.com>
2020-05-16 18:56:26 +0100

    Fix missing subtraction operator

--- scope.js
+++ scope.js
@@ -3,7 +3,7 @@ const topScope = Object.create(null);
 topScope.true = true;
 topScope.false = false;
 
-for (let op of ["+", "*", "/", "==", "<", ">"]) {
+for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
     topScope[op] = Function("a, b", `return a ${op} b;`);
 }
 
